d9b220ec7cf55ef1804990acf818defa322019da
Babitha17 <gandikotababitha79@gmail.com>
2025-09-25 11:15:30 +0530

    Add files via upload

---
+++ week 4/add.js
@@ -0,0 +1,20 @@
+let a = 10, b = 3;
+console.log(a + b);  
+console.log(a - b);  
+console.log(a * b);  
+console.log(a / b);  
+console.log(a % b);  
+console.log(5 == '5');   
+console.log(5 === '5');  
+console.log(7 > 3);     
+console.log(7 <= 3);     
+
+console.log(a ** b); 
+let x = 5;
+x += 2; 
+x *= 3; 
+console.log(5 & 1); 
+console.log(5 | 1);  
+console.log(5 ^ 1);  
+console.log(~5);     
+console.log
